7dd02b0be6338d1df0cbbfe0197b55923f0ac2a7
Pier <madstrum@gmail.com>
2018-05-30 14:21:56 +0800

    Changed to sample app

--- README.md
+++
@@ -1,3 +0,0 @@
-# Picsharz_CodeDeploy
-Working AWS Code Deploy Linked Repository for Picsharz Project
-Touch
--- js/login.js
+++
@@ -1,7 +0,0 @@
-(function(ready, EventEmitter) {
-
-  ready(function() {
-    EventEmitter.emit('LoginForm:mount')
-  })
-
-})(window.ready, window.EventEmitter)
